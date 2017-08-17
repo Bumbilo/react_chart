df5c40716d1b082df9668fd466380abeeb4609e0
Dmitriy Golyak <dimagolyak@gmail.com>
2017-08-17 22:15:12 +0300

    new change add styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './app'
+import Chat from 'containers/Chat'
+import 'assets/styles/style.scss'
+import { AppContainer } from 'react-hot-loader'
 
-render(
-	<App />,
-	document.querySelector('#mount_place')
-)
+
+const renderApp = Component => {
+	render(
+		<AppContainer>
+			<Component />
+		</AppContainer>,
+		document.querySelector('#mount_place')
+	)
+}
+
+
+renderApp(Chat)
+
+
+if(module.hot) {
+	module.hot.accept('containers/Chat', () => { renderApp(Chat) })
+}
